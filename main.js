c2ed086a8ab58fe6a70dbd4f1f0cb0e165d69266
agos <agosnoriega2018@gmail.com>
2025-01-26 15:57:44 -0300

    subiendo version 2.0 beta

--- main.js
+++ main.js
@@ -1,140 +1,143 @@
+const contenedorProdDest = document.querySelector(".productos");
+//botones de filtrado
+const btnCargarProdDest = document.getElementById("btnCargProdDest");
+const btnCargarProdNorm = document.getElementById("btnCargProdNorm");
+const btnCargarTdProd =document.getElementById("btnCargTdProd");
+let cartaProducto
+let btnComprar
+let objCompr
+let carrito
+let arrProductos =[]
+let arrCarrito=[]
+let prodMostr=[]
+let prodSubir =[]
 
-const contenedorProd = document.querySelector(".productos");
-const productosLocal = JSON.parse(localStorage.getItem('productos'));
-const carritoLocal = JSON.parse(localStorage.getItem('carrito'));
+const obtenerProductos  = async () =>{
+    if(!localStorage.getItem("productos")){
+    const respuesta = fetch(`./productos.json`)
+    .then(respuesta => respuesta.json())
+    .then(productos =>{
+        arrProductos=productos
+        console.log(arrProductos)
 
-const comprar = (prod) =>{
-    productosLocal.forEach((producto) =>{
-        if(producto.id == prod & producto.stock >0 ){
-            producto.stock -= 1;
-            Swal.fire({
-                icon:"success",
-                title:"Producto agregado al carrito",
-                
-            })
-            console.log(producto.stock)
-            carritoLocal.forEach((producto)=>{
-                if(producto.id == prod){
-                    producto.enCarro +=1;
+    })
+    .catch(error => console.log("ERROR"))
+}
+else{
+    arrProductos = JSON.parse(localStorage.getItem("productos"))
+}
+console.log(arrProductos)
+}
+const obtenerCarrito = async () =>{
+    if(!localStorage.getItem("carrito")){
+    const respuesta = fetch(`./carrito.json`)
+    .then(respuesta => respuesta.json())
+    .then(productos =>{
+        arrCarrito=productos
+    })
+    .catch(error => console.log("ERROR"))
+}
+else{
+    arrCarrito = JSON.parse(localStorage.getItem("carrito"));
+}
+}
+const cargarEnLocalStorage = () => {
+    localStorage.setItem('carrito',JSON.stringify(arrCarrito));
+    localStorage.setItem('productos',JSON.stringify(arrProductos));
+    return mostrar();
+}  
+obtenerProductos();
+obtenerCarrito();
+
+
+btnCargarProdDest.onclick =()=>{
+    console.log("destacado");
+    prodMostr=[];
+    prodMostr = arrProductos.filter(producto => producto.tipo === 'destacado' && producto.stock >0);
+    return mostrar();
+}
+btnCargarProdNorm.onclick =() =>{
+    console.log("normal");
+    
+    prodMostr=[];
+    prodMostr = arrProductos.filter(producto => producto.tipo === 'normal' && producto.stock >0);
+    return mostrar();
+}
+btnCargarTdProd.onclick =() =>{
+    prodMostr=[];
+    prodMostr = arrProductos;
+    return mostrar();
+}
+
+const mostrarInicio =() =>{
+    prodMostr=[];
+    prodMostr =arrProductos;
+    return mostrar();
+}
+
+const mostrar = () =>{
+    contenedorProdDest.innerHTML ="";
+    prodMostr.forEach(producto=> {
+        if(producto.stock >0){
+        contenedorProdDest.innerHTML +=`
+                    <div class="producto">
+            <img src="${producto.imagen}" alt="imagenProducto" class="imagenProducto">
+
+            <h2 class="tituloProducto">${producto.nombre}</h2>
+
+            <p class="descripcionProducto">${producto.descripcion}</p>
+
+            <p class="precioProducto">precio: ${producto.precio}$</p>
+
+            <h3 class="stock">stock: ${producto.stock}</h3>
+
+             <button class="btnAgregar" id="${producto.id}">Agregar al carrito</button>
+
+        </div>
+        `}
+});
+return obtenerBtns ();
+}
+const obtenerBtns =() =>{
+    btnComprar = Array.from(document.getElementsByClassName("btnAgregar"));
+    cartaProducto = document.querySelector(".producto");
+    console.log("botones", btnComprar)
+console.log("cartas",cartaProducto);
+}
+
+console.log("botones", btnComprar)
+console.log("cartas",cartaProducto);
+
+contenedorProdDest.onclick = (e) =>{
+    console.log(e);
+    objCompr = btnComprar.find((boton) => boton.id == e.target.id)
+    console.log("objetivo",objCompr)
+    if(objCompr !="" && objCompr != undefined){
+        console.log(objCompr.id);
+        return descontarStock();
+    }
+}
+const descontarStock = () =>{
+    arrProductos.forEach((producto) =>{
+        if(producto.id == objCompr.id && producto.stock>0){
+            producto.stock--;
+            arrCarrito.forEach(productoCarrito => {
+                if(producto.id == productoCarrito.id){
+                    Swal.fire({
+                        icon:"success",
+                        title: "producto agregado con éxito",
+                    })
+                    productoCarrito.unidad++;
+
+                    console.log("compra realizada")
                 }
             })
-            carritoLocal.forEach((producto) =>{
-                console.log(`Nombre:${producto.nombre} en carro: ${producto.enCarro}`)
-            })
-            localStorage.setItem('productos',JSON.stringify(productosLocal))
-            localStorage.setItem('carrito',JSON.stringify(carritoLocal))
-            return actualizarProductos();
+            console.log(arrCarrito)
+            return cargarEnLocalStorage();
+            
         }
+
     })
-}
-const sinProductos= () =>{
-    let productosEncontrados=0
-    productosLocal.forEach((prod) =>{
-        if(prod.stock >0){
-            productosEncontrados+=1}
-     })
-    if(productosEncontrados<=0){
-        contenedorProd.innerHTML += `
-        <div class="pantallaSinProd">
-        <h2 class="informe">No hay productos disponibles</h2>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_0sFRH4yVvQI88wdwhC8topZE2xVCQH1FLg&s">
-        </div>`
-     }
-     console.log(productosEncontrados)
-}
-const actualizarProductos = () =>{
-    contenedorProd.innerHTML="";
-
-    productosLocal.forEach((producto) =>{
-        if (producto.stock > 0) {
-            contenedorProd.innerHTML +=`
-            <div class="producto">
-                <img src="${producto.imagen}" alt="imagenProducto" class="imagenProducto">
-
-                <h2 class="tituloProducto">${producto.nombre}</h2>
-
-                <p class="descripcionProducto">${producto.descripcion}</p>
-
-                <p class="precioProducto">precio: ${producto.precio}$</p>
-
-                <h3 class="stock">stock: ${producto.stock}</h3>
-
-                 <button class="btnAgregar" id="${producto.id}">Agregar al carrito</button>
-
-            </div>
-            `
-        }
-})
-
- btnComprar = Array.from(document.getElementsByClassName("btnAgregar"));
- sinProductos()
- console.log(contenedorProd);  
 
 }
-const mostrarProductos = () => {
-
-    productosLocal.forEach((element) => {
-
-        if (element.stock > 0) {
-
-            contenedorProd.innerHTML += `
-
-             <div class="producto">
-
-                    <img src="${element.imagen}" alt="imagenProducto" class="imagenProducto">
-
-                    <h2 class="tituloProducto">${element.nombre}</h2>
-
-                    <p class="descripcionProducto">${element.descripcion}</p>
-
-                    <p class="precioProducto">precio: ${element.precio}$</p>
-
-                    <h3 class="stock">stock: ${element.stock}</h3>
-
-                    <button class="btnAgregar" id="${element.id}">Agregar al carrito</button>
-
-            </div>
-
-            `;
-
-        }
-
-    });
-     btnComprar = Array.from(document.getElementsByClassName("btnAgregar")); // Convertir a array
-
-};
-
-let btnComprar = Array.from(document.getElementsByClassName("btnAgregar")); // Convertir a array
-
-
-mostrarProductos();
-
-
-
-contenedorProd.onclick = (e) => {
-
-    console.log(e.target);
-
-    let btnLocalizado = 0;
-
-
-    btnComprar.forEach((btn) => {
-
-        console.log("click");
-
-        if (btn=== e.target) {
-
-            btnLocalizado = btn.id; // O cualquier otra acción que desees realizar
-
-            console.log(`Botón localizado: ${btnLocalizado}`);
-
-            return comprar(btnLocalizado); // Salir del bucle forEach
-
-        }
-    });
-
-};
-
-
-console.log(btnComprar);
-sinProductos();
+mostrarInicio();
